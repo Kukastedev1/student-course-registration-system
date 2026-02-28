52d5ed00c6145c059f69264bd0d28a445fad5b97
Albertinez <albertmochama@gmail.com>
2026-02-28 15:30:53 +0300

    Implemented student and course models

--- models/student.py
+++ models/student.py
@@ -1,9 +1,24 @@
-"""
-Student Model
+import json
 
-Represents a student in the system.
+class Student:
+    def __init__(self, student_id, name, email):
+        self.student_id = student_id
+        self.name = name
+        self.email = email
+        self.courses = []  # list of course_ids
 
-Responsibilities:
-- Store student details
-- Track courses the student is enrolled in
-"""
+    def enroll(self, course_id):
+        if course_id not in self.courses:
+            self.courses.append(course_id)
+
+    def drop_course(self, course_id):
+        if course_id in self.courses:
+            self.courses.remove(course_id)
+
+    def to_dict(self):
+        return {
+            "student_id": self.student_id,
+            "name": self.name,
+            "email": self.email,
+            "courses": self.courses
+        }
