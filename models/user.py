03026dbd0c92db0c633d6568c3c22d954d7f6745
Athanas Mochama <athanasmochama253@gmail.com>
2026-02-28 18:53:01 +0300

    Implemented authentication system with User model and AuthService

--- models/user.py
+++ models/user.py
@@ -1,10 +1,28 @@
-"""
-User Model
+import hashlib
 
-This class represents users in the system.
-Users can be either admin or student.
 
-Responsibilities:
-- Store login credentials
-- Provide authentication support
-"""
+class User:
+    def __init__(self, user_id, username, password):
+        self.id = user_id
+        self.username = username
+        self.__password = self.__hash_password(password)
+
+    # Encapsulation: private password
+    def __hash_password(self, password):
+        return hashlib.sha256(password.encode()).hexdigest()
+
+    def check_password(self, password):
+        return self.__password == hashlib.sha256(password.encode()).hexdigest()
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "password": self.__password
+        }
+
+    @staticmethod
+    def from_dict(data):
+        user = User(data["id"], data["username"], data["password"])
+        user._User__password = data["password"]
+        return user
